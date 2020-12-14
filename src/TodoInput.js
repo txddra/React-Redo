3f29d9e52043123c66a9cfa83a808ec4f50d6456
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-13 20:36:29 -0600

    testing context, app still functional

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,29 +1,11 @@
 import React, { useState, useContext } from "react";
-import {TodoInputContext} from "./context/context"
-import Todo from "./Todo"
+
+import TodoInputDemo from "./context/TodoInputDemo"
 
 const TodoInput =()=>{
 
-    const { addTodo } = useContext(TodoInputContext)
-    const [todo, setTodo] = useState("");
-
-    function handleCreateNewTodo(e) {
-       e.preventDefault()
-
-        addTodo(todo)
-    }
-
     return (
-        <form onSubmit={handleCreateNewTodo}>
-            <input 
-            type ="text"
-            value={todo}
-            onChange={(e) =>setTodo(e.target.value)}
-            />
-
-            <button>Submit</button>
-
-        </form>
+        <TodoInputDemo />
     )
 };
 
---
+++ src/context/TodoInputDemo.js
@@ -0,0 +1,30 @@
+import React, { useState, useContext } from "react";
+import {TodoInputContext} from "../context/context"
+// import Todo from "./Todo"
+
+const TodoInputDemo =()=>{
+
+    const { addTodo } = useContext(TodoInputContext)
+    const [todo, setTodo] = useState("");
+
+    function handleCreateNewTodo(e) {
+       e.preventDefault()   
+
+        addTodo(todo)
+    }
+
+    return (
+        <form onSubmit={handleCreateNewTodo}>
+            <input 
+            type ="text"
+            value={todo}
+            onChange={(e) =>setTodo(e.target.value)}
+            />
+
+            <button>Submit</button>
+
+        </form>
+    )
+};
+
+export default TodoInputDemo;
