d35b5128205bf2d65a7653df0095f770189e3d90
LaToddra Belton <latoddra.belton@codeimmersives.com>
2020-12-13 19:40:57 -0600

    done button functional

--- src/Todo.js
+++ src/Todo.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
-const Todo = ({ todoList: {todo,id }, removeTodo}) =>{
-    return(
-        <div>
+const Todo = ({ todoList: {todo,id, isComplete}, removeTodo, completeTodo}) =>{
+
+    // function  deleteByID() {
+    //     removeTodo(id)
+    // }
+    return( 
+        <div style ={{textDecoration: isComplete ? "line-through" : ""}}>
 {/* destructured */}
-            {todo}
+            {todo}  
+            {/* if the todo was completed, line through it, otherwise return an empty string*/}
             <div>
-                <button>Done</button>
+                <button onClick ={()=>completeTodo(id)}>Done</button>
                 <button onClick={()=> removeTodo(id)}>Delete</button>
                 </div>
                 <hr />
